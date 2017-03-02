cb555c97a64ec17e47cb14f23f7bda6d8fe1c346
gmarler <gmarler@gmarler.com>
2017-03-01 23:24:24 -0500

    add gensquares.py

---
+++ labs/py3/generators/gensquares.py
@@ -0,0 +1,12 @@
+def gen_squares(max_root):
+  for n in range(max_root):
+    yield n ** 2
+
+squares = gen_squares(5)
+
+# for square in squares: print(square)
+
+print(next(squares))
+print(next(squares))
+print(next(squares))
+print(next(squares))
